80e6708f24f83b4e09663033035045248c1eea87
sushil pawar <sushil.pawar@talentica.com>
2017-12-18 12:24:38 +0530

    change4 on 2.1

--- solr-demo/src/main/java/com/demo/test/Book.java
+++ solr-demo/src/main/java/com/demo/test/Book.java
@@ -9,7 +9,6 @@ import org.springframework.data.solr.core.mapping.SolrDocument;
 @SolrDocument(solrCoreName = "profile")
 public class Book {
 	
-	//sushil comment
 	@Id
     @Field
     private String id;
